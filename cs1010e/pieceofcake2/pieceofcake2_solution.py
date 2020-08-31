36b0db940fb823526a58d5e190608f44745c4380
yonggqiii <yongqifoo@gmail.com>
2020-08-31 21:49:54 +0800

    Cleaned data files and more shtuff

---
+++ cs1010e/pieceofcake2/pieceofcake2_solution.py
@@ -0,0 +1,16 @@
+def piece_of_cake_2(n: int, h: int, v: int) -> int:
+    '''
+    https://open.kattis.com/problems/pieceofcake2
+    Define the piece_of_cake_2 function which solves the problem
+    given n, h and v.
+    '''
+    return 4 * max(n - h, h) * max(n - v, v)
+
+### DO NOT EDIT THE PROGRAM BEYOND THIS LINE ###
+def main():
+    n, h, v = map(lambda x: int(x), input().split(' '))
+    print(piece_of_cake_2(n, h, v))
+
+if __name__ == '__main__':
+    main()
+
---
+++ cs1010e/planina/planina_template.py
@@ -0,0 +1,14 @@
+def planina(n: int) -> int:
+    '''
+    https://open.kattis.com/problems/planina
+    Define the planina function which solves the problem given N.
+    '''
+    pass
+
+### DO NOT EDIT THE PROGRAM BEYOND THIS LINE ###
+def main():
+    print(planina(int(input())))
+
+if __name__ == '__main__':
+    main()
+
